57877a2a0f1002ef857e21aaff836ecdfd456867
Harika Rajavaram <harika_rajavaram@yahoo.com>
2018-11-30 04:42:55 +0530

    add test file

---
+++ test.c
@@ -0,0 +1,24 @@
+#include "sbox.c"
+
+//prints both sboxes got from code and from the actual sbox array
+int test(int x, int d){
+	unsigned int *shares = share(x,d);
+	//calling sbox on shares of x
+	unsigned int *ans = sbox(shares,d);
+	max = (int) pow(2,N);
+	printf("printing sbox shares\n");
+	for(unsigned int f=0;f<d;f++){
+		printf("%d ", ans[f]);
+	}
+	printf("\n");
+	// decoding the shares got from sbox evaluation
+	unsigned int sbox_code = decode(ans,d);
+	printf("sbox evaluation by program is %d\n",sbox_code);
+	printf("actual sbox from array is %d\n", s[x]);
+}
+
+int main(){
+	int x = 100;
+	int d = 4;
+	test(x,d);
+}
